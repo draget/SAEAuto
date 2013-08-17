f06f0c58a9d418e8533f0f27bea9ef098697ae5b
Thomas H. Drage <tomlm14>
2013-08-17 12:58:32 +0800

    Fix merge issue

--- TestCode/Control/Xsens.cpp
+++ TestCode/Control/Xsens.cpp
@@ -70,7 +70,7 @@ bool Xsens::Open() {
 		int timeOut = 100;
 		if (serial->setTimeoutMeasurement(timeOut) != XRV_OK) { Log->WriteLogLine("XSens - Couldn't set timeout."); return false; }
 	
-    		CmtDeviceMode mode(CMT_OUTPUTMODE_ORIENT | CMT_OUTPUTMODE_CALIB, CMT_OUTPUTSETTINGS_TIMESTAMP_SAMPLECNT | CMT_OUTPUTSETTINGS_COORDINATES_NED |CMT_OUTPUTSETTINGS_ORIENTMODE_EULER | CMT_OUTPUTSETTINGS_CALIBMODE_ACCGYR, 100);
+    		CmtDeviceMode mode(CMT_OUTPUTMODE_ORIENT | CMT_OUTPUTMODE_CALIB, CMT_OUTPUTSETTINGS_TIMESTAMP_SAMPLECNT | CMT_OUTPUTSETTINGS_COORDINATES_NED |CMT_OUTPUTSETTINGS_ORIENTMODE_MATRIX | CMT_OUTPUTSETTINGS_CALIBMODE_ACCGYR, 100);
     		if (serial->setDeviceMode(mode, false, CMT_DID_BROADCAST)) { Log->WriteLogLine("XSens - Failed to set device mode."); return false; }
 
 		if (serial->gotoMeasurement()) { Log->WriteLogLine("XSens - Failed to set device mode."); return false; }
@@ -134,7 +134,7 @@ void Xsens::ProcessMessages() {
 	zacc_comp = (long double) matrix_data.m_data[0][2]*xacc + matrix_data.m_data[1][2]*yacc + matrix_data.m_data[2][2]*zacc;
 
 
-<<<<<<< HEAD
+
 	//long double xacc_comp = (long double) cos_pitch*xacc + sin_pitch*zacc;
 	//long double yacc_comp = (long double) sin_pitch*sin_roll*xacc + cos_roll*yacc - sin_roll*cos_pitch*zacc;
 	//long double zacc_comp = (long double) -cos_roll*sin_pitch*xacc + sin_roll*yacc + cos_roll*cos_pitch*zacc;
@@ -145,10 +145,10 @@ void Xsens::ProcessMessages() {
 	else { Yaw = 360 + 360*asinl(matrix_data.m_data[1][0])/2/M_PI; }
 
 	IMULog->WriteLogLine(boost::lexical_cast<std::string>(CarControl->TimeStamp()) + "," + boost::lexical_cast<std::string>(Yaw) + "," + boost::lexical_cast<std::string>(xacc_comp) + "," + boost::lexical_cast<std::string>(yacc_comp));
-=======
-	if(reply->getOriEuler().m_yaw < 0) { Yaw = 360 + reply->getOriEuler().m_yaw ; }
-	else { Yaw = reply->getOriEuler().m_yaw; }
->>>>>>> c56f927f80684903b6d138e490856acc45ca5602
+
+//	if(reply->getOriEuler().m_yaw < 0) { Yaw = 360 + reply->getOriEuler().m_yaw ; }
+//	else { Yaw = reply->getOriEuler().m_yaw; }
+
 
     }
 
