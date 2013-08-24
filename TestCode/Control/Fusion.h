d50b96c5dedfd8d3e65f4b589e3286bb627a08f7
Thomas H. Drage <tomlm14>
2013-08-24 15:16:50 +0800

    New

---
+++ TestCode/Control/Fusion.h
@@ -0,0 +1,49 @@
+
+#ifndef _Fusion_H
+#define	_Fusion_H
+
+#define STEERING_CONSTANT 0.157
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "Control.h"
+
+class KalmanPVA;
+
+class Logger;
+
+class Fusion {
+public:
+
+	Fusion(Control* CarController, Logger* CarLogger);
+    	Fusion(const Fusion& orig);
+    	virtual ~Fusion();
+
+	VECTOR_2D CurrentVelocity;
+	VECTOR_2D CurrentPosition;
+	double CurrentHeading;
+	double CurrentSpeed;
+
+	void GPSUpdate(VECTOR_2D GPSPosition, double GPSSpeed);
+	void GPSTrackAngleUpdate(double GPSTrackAngle);
+
+private:
+
+	Control* CarControl;
+
+	Logger* Log;
+
+	KalmanPVA *VAKlmX;
+	KalmanPVA *VAKlmY;
+	KalmanPVA *PVKlmX;
+	KalmanPVA *PVKlmY;
+
+	Logger* FusionLog;
+
+
+};
+
+#endif	/* _Fusion_H */
+
